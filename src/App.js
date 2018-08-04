41f58cf8c5edaa4b75a95d3aeccfbced69d5c8e9
Prabeen04 <prabeen.strange@gmail.com>
2018-08-04 23:36:14 +0530

    Chat component creacted

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Chat/>
       </div>
     );
   }
 }
 
 export default App;
+
+const Chat = (props) => {
+  return(
+    <div>
+      <h3>chat component</h3>
+      <input type="text" value={props.message}/>
+      <button>Send</button>
+    </div>
+  )
+}
